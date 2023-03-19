ce95033a579b3d6dd1ddb58587a172079e8487ec
VnPower <vnpower@disroot.org>
2023-03-19 16:46:49 +0700

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../app.css";
+  import Header from "./Header.svelte"
 </script>
 
 <div class="">
+  <Header />
   <slot></slot>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,6 @@
   <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
     Hello!
   </h1>
-  <p class="text-center">And goodbye because nothing is here!</p>
+  <p class="text-center">And goodbye because nothing is here yet!</p>
+  <p class="text-center">I am lazy as hell!!</p>
 </main>
