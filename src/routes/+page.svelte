ab159a0dd52b8543f082a83d2144735de0f7b48f
VnPower <vnpower@disroot.org>
2023-03-19 16:23:13 +0700

    asd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+    Hello!
+  </h1>
+  <p class="text-center">And goodbye because nothing is here!</p>
+</main>
